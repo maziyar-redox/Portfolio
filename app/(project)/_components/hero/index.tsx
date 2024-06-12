f81d39e7f75713d5fd517b1b7d2f0f7dd7bdc0f8
Maziyar <mr_red0x1010@yahoo.com>
2024-06-12 18:45:53 +0330

    Fixed production error

--- app/(project)/_components/hero/index.tsx
+++ app/(project)/_components/hero/index.tsx
@@ -5,7 +5,7 @@ export function Hero() {
                 <span className="text-red-600">Empowering Your</span> Digital Vision
             </h1>
             <p className="md:text-sm text-xs font-normal text-white-55 text-center md:text-start">
-            At DigitX, we are committed to empowering your digital vision with innovative solutions tailored to your unique needs. Our team of skilled professionals, coupled with cutting-edge technologies, ensures that we deliver exceptional results that elevate your online presence. Whether it's captivating web designs, seamless app development, effective digital marketing, or other digital solutions, we are dedicated to bringing your ideas to life and helping your business thrive in the dynamic digital landscape. Partner with us today, and let's unlock endless possibilities for your digital success!
+            At DigitX, we are committed to empowering your digital vision with innovative solutions tailored to your unique needs. Our team of skilled professionals, coupled with cutting-edge technologies, ensures that we deliver exceptional results that elevate your online presence. Whether it&apos;s captivating web designs, seamless app development, effective digital marketing, or other digital solutions, we are dedicated to bringing your ideas to life and helping your business thrive in the dynamic digital landscape. Partner with us today, and let&apos;s unlock endless possibilities for your digital success!
             </p>
         </div>
     );
