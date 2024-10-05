6d86a819bcaf61abd58ecc8a747aa2d6b0c80a4a
Maziyar <mr_red0x1010@yahoo.com>
2024-10-05 16:17:58 +0330

    Added Meta to layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -6,30 +6,30 @@ export const metadata: Metadata = {
     title: "MrRedox Portolio | Home Page",
     description: "Full-Stack Developer and computer engineering student.",
     openGraph: {
-        title: "MrRedox Portolio | Home Page",
+        title: "Home Page",
         description: "Full-Stack Developer and computer engineering student.",
         url: 'https://maziyar-isa.ir/',
         siteName: "MrRedox Portfolio",
         type: "website",
         images: [
             {
-              url: 'https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png',
-              secureUrl: 'https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png',
+              url: 'https://raw.githubusercontent.com/maziyar-redox/my-blog-posts/refs/heads/main/img/home.png',
+              secureUrl: 'https://raw.githubusercontent.com/maziyar-redox/my-blog-posts/refs/heads/main/img/home.png',
               width: 1200,
               height: 630,
-              alt: 'Preview image for MrRedox',
+              alt: 'Preview image for home',
             }
         ]
     },
     twitter: {
         card: 'summary_large_image',
-        site: '@yesdavidgray',
-        title: "MrRedox",
-        description: "Hello, I'm Dave. 👋 I teach coding and web development skills.",
-        creator: '@yesdavidgray',
+        site: 'https://maziyar-isa.ir/',
+        title: "Home Page",
+        description: "Full-Stack Developer and computer engineering student.",
+        creator: '@maziyar-redox',
         images: {
-            url: 'https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png',
-            alt: 'Preview image for MrRedox',
+            url: 'https://raw.githubusercontent.com/maziyar-redox/my-blog-posts/refs/heads/main/img/home.png',
+            alt: 'Preview image for home',
         }
     },
 };
