26d3e3f00c157576cb2685dc26dcff11bbb6658e
maziyar <r6.acc.051@gmail.com>
2026-08-26 21:39:51 +0330

    Initiated multi-app application

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+
+const root = resolve(__dirname, "src");
+const outdir = resolve(__dirname, "dist");
 
 // https://vite.dev/config/
 export default defineConfig({
+  root,
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  build: {
+    outDir: outdir,
+    emptyOutDir: true,
+    rolldownOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        nested: resolve(root, "nested", "index.html")
+      },
+    },
+  },
 })
